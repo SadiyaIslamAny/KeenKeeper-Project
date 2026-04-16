41e69f5b16ecd508d91db66d489e6d394802fbc3
SADIA <you@example.com>
2026-04-16 18:18:18 +0600

    add yourFriends component

--- src/Components/HomePage/YourFriends.jsx
+++ src/Components/HomePage/YourFriends.jsx
@@ -1,17 +1,95 @@
-import React, { use } from 'react';
-
-const friendsPromise = fetch('../../../public/friends.json').then(res=> res.json());
-
+import React, { useEffect, useState } from 'react';
 const YourFriends = () => {
-    const friend = use(friendsPromise);
-    console.log(friend)
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('/public/friends.json')
+            const data = await res.json()
+            setFriends(data);
+        };
+        fetchData();
+    }, [])
+
+
+    const StatusColor = (status) => {
+        if (status === "Overdue") {
+            return "bg-[#EF4444] text-white";
+        }
+        else if(status === "Almost due") {
+            return "bg-[#EFAD44] text-white";
+        }
+        return "bg-[#244D3F] text-white";
+    };
+
+    console.log(friends);
     return (
-    <div className="bg-[#F8FAFC]">
-        <div className='w-10/12 mx-auto'>
-            <h2>Your Friends</h2>
-            
+        <div className="bg-[#F8FAFC] py-10">
+            <div className="w-10/12 mx-auto mb-4">
+                <h2 className="text-2xl font-semibold">
+                    Your Friends
+                </h2>
+            </div>
+
+            <div className="w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+
+                {friends.map((friend) => (
+
+                    <div
+                        key={friend.id} 
+                        className="card bg-[#FFFFFF] w-full shadow-sm rounded-xl"
+                    >
+
+                       
+                        <figure className="pt-6 flex justify-center">
+                            <img
+                                src={friend.picture}
+                                alt={friend.name}
+                                className="rounded-full w-24 h-24 object-cover"
+                            />
+                        </figure>
+
+                        <div className="mx-auto p-4">
+
+                            <h2 className="font-semibold text-xl text-center">
+                                {friend.name}
+                            </h2>
+
+                           
+                            <p className="text-center text-gray-500">
+                                {friend.days_since_contact} Day
+                            </p>
+
+                            <div className="flex flex-wrap justify-center gap-2 mt-2">
+                                {friend.tags.map((tag, index) => (
+                                    <span
+                                        key={index}
+                                        className="bg-[#CBFADB] text-[#244D3F] px-3 py-1 font-medium rounded-full text-sm"
+                                    >
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                            <div
+
+                                className={`px-3 py-1 rounded-full text-sm font-medium w-25 text-center mx-auto mt-3 ${StatusColor(
+                                    friend.status
+                                )}`}
+                            >
+                                {friend.status}
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                ))}
+
+            </div>
+
         </div>
-        </div>
+
     );
 };
 
